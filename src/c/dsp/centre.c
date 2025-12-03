e28b53ef271508c8f1457d61d877f4ab72ac3981
Aris Lorenzo <lnotsira@gmail.com>
2025-12-03 13:47:24 +0000

    dsp: add some steering logic for center
    Signed-off-by: Aris Lorenzo <lnotsira@gmail.com>

--- src/c/dsp/centre.c
+++ src/c/dsp/centre.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include <centre.h>
 
 void
@@ -11,13 +13,24 @@ build_centre_from_stereo(L, R, C, frames, gain, subtract)
 {
 	size_t i;
 
+	const float eps = 1e-6f;
+	const float steer_f = 1.5f;
+
 	for (i = 0; i < frames; i++) {
 		float l = L[i];
 		float r = R[i];
 
 		float mid = 0.5f * (l + r);
+		float side = 0.5f * (l + r);
 
-		float sub = subtract * mid;
+		float mid_abs = fabsf(mid);
+		float side_abs = fabsf(side);
+
+		float ratio = mid_abs / (side_abs + eps);
+		float steer = ratio * steer_f;
+		if (steer > 1.0f) steer = 1.0f;
+
+		float sub = subtract * steer * mid;
 
 		L[i] = l - sub;
 		R[i] = r - sub;
